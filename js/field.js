164be09e8d9a8eac6b750b2127cf22810b690dfe
duartegalvao <duartegalvao@tecnico.ulisboa.pt>
2017-10-02 20:25:19 +0100

    > precisams de tors mais pequens
    circulo interior

--- js/field.js
+++ js/field.js
@@ -1,6 +1,7 @@
 var TORUS_SIZE = 6;
 var TORUS_RADIUS = 2;
 var TORUS_OUTSIDE_MARGIN = 10;
+var TORUS_INSIDE_MARGIN = 130;
 var TORUS_X_N = 30;
 var TORUS_Z_N = 15;
 
@@ -8,7 +9,7 @@ function addFieldTorus(obj, x, y, z) {
   'use strict';
   geometry = new THREE.TorusGeometry(TORUS_SIZE, TORUS_RADIUS, 16, 32);
   mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(x, y, z);
+  mesh.position.set(x, y + TORUS_RADIUS, z);
   mesh.rotation.x = Math.PI / 2;
   obj.add(mesh);
 }
@@ -30,12 +31,30 @@ function createField(x, y, z) {
   let fieldLength = TABLE_SIZEX - 2*k;
   let fieldWidth = TABLE_SIZEZ - 2*k;
 
-  for (let x = -fieldLength / 2; x <= fieldLength / 2; x += fieldLength / TORUS_X_N) {
+  let n = 30;
+  for (let x = -fieldLength / 2; x <= fieldLength / 2; x += fieldLength / n) {
       addFieldTorus(field, x, 0, fieldWidth / 2);
       addFieldTorus(field, x, 0, -fieldWidth / 2);
   }
 
-  for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / TORUS_Z_N) {
+  n = 15;
+  for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / n) {
+      addFieldTorus(field, fieldLength / 2, 0, z);
+      addFieldTorus(field, -fieldLength / 2, 0, z);
+  }
+
+  k = TORUS_INSIDE_MARGIN + TORUS_SIZE;
+  fieldLength = TABLE_SIZEX - 2*k;
+  fieldWidth = TABLE_SIZEZ - 2*k;
+
+  n = 20;
+  for (let x = -fieldLength / 2; x <= fieldLength / 2; x += fieldLength / n) {
+      addFieldTorus(field, x, 0, fieldWidth / 2);
+      addFieldTorus(field, x, 0, -fieldWidth / 2);
+  }
+
+  n = 5;
+  for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / n) {
       addFieldTorus(field, fieldLength / 2, 0, z);
       addFieldTorus(field, -fieldLength / 2, 0, z);
   }
