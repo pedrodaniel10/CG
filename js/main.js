1eb3e1fb6ab85b674efc7d58beff3680d32190ca
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2017-10-02 16:15:15 +0100

    Table

---
+++ js/main.js
@@ -0,0 +1,73 @@
+var camera, scene, renderer;
+
+var geometry, material, mesh;
+
+function render() {
+  'use strict';
+  renderer.render(scene, camera);
+}
+
+function createCamera() {
+  'use strict';
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+
+  camera.position.x = 50;
+  camera.position.y = 50;
+  camera.position.z = 50;
+  camera.lookAt(scene.position);
+}
+
+
+function createScene() {
+  'use strict';
+  scene = new THREE.Scene();
+
+  scene.add(new THREE.AxisHelper(10));
+  createTable(0, 0, 0);
+}
+
+function onResize() {
+  'use strict';
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  if (window.innerHeight > 0 && window.innerWidth > 0) {
+    camera.aspect = renderer.getSize().width / renderer.getSize().height;
+    camera.updateProjectionMatrix();
+  }
+}
+
+function onKeyDown(e) {
+  'use strict'
+  switch (e.keyCode) {
+
+    case 65: //A
+    case 97: //a
+      scene.traverse(function (node) {
+        if (node instanceof THREE.Mesh) {
+          node.material.wireframe = !node.material.wireframe;
+        }
+      });
+      break;
+  }
+}
+
+function animate() {
+  'use strict';
+
+  render();
+  requestAnimationFrame(animate);
+}
+
+function init() {
+  'use strict';
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+  createCamera();
+  render();
+
+  window.addEventListener("resize", onResize);
+  window.addEventListener("keydown", onKeyDown)
+}
