325ced83a6b740ecaaf1e5218ece8574b4619c3b
duartegalvao <duartegalvao@tecnico.ulisboa.pt>
2017-10-02 20:04:33 +0100

    toros à volta

--- js/field.js
+++ js/field.js
@@ -1,5 +1,8 @@
-var TORUS_SIZE = 10;
-var TORUS_RADIUS = 3;
+var TORUS_SIZE = 6;
+var TORUS_RADIUS = 2;
+var TORUS_OUTSIDE_MARGIN = 10;
+var TORUS_X_N = 30;
+var TORUS_Z_N = 15;
 
 function addFieldTorus(obj, x, y, z) {
   'use strict';
@@ -23,7 +26,19 @@ function createField(x, y, z) {
   var field = new THREE.Object3D();
   material = new THREE.MeshBasicMaterial({ color: 0xe68200, wireframe: true });
 
-  addFieldTorusStack(field, 5, 0, -1);
+  let k = TORUS_OUTSIDE_MARGIN + TORUS_SIZE;
+  let fieldLength = TABLE_SIZEX - 2*k;
+  let fieldWidth = TABLE_SIZEZ - 2*k;
+
+  for (let x = -fieldLength / 2; x <= fieldLength / 2; x += fieldLength / TORUS_X_N) {
+      addFieldTorusStack(field, x, 0, fieldWidth / 2);
+      addFieldTorusStack(field, x, 0, -fieldWidth / 2);
+  }
+
+  for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / TORUS_Z_N) {
+      addFieldTorusStack(field, fieldLength / 2, 0, z);
+      addFieldTorusStack(field, -fieldLength / 2, 0, z);
+  }
 
   scene.add(field);
   field.position.x = x;
