c8721d521bf335b638cc78c630b21ab962949d24
duartegalvao <duartegalvao@tecnico.ulisboa.pt>
2017-10-04 14:31:15 +0100

    afinal sou autista (:

--- js/Cheerio.js
+++ js/Cheerio.js
@@ -6,6 +6,6 @@ class Cheerio extends Mesh {
         this.geometry = new THREE.TorusGeometry(s, r, 16, 32);
         this.material = new THREE.MeshBasicMaterial({ color: 0xe68200, wireframe: true});
         this.rotation.x = Math.PI / 2;
-        this.position.y = y + r
+        this.position.y = y + r;
     }
 }
--- js/field.js
+++ js/field.js
@@ -12,35 +12,35 @@ class Field extends Object3D {
         this.TORUS_Z_N = 15;
 
         let k = this.TORUS_OUTSIDE_MARGIN + this.TORUS_SIZE;
-        let fieldLength = this.TABLE_SIZEX - 2*k;
-        let fieldWidth = this.TABLE_SIZEZ - 2*k;
+        let fieldLength = TABLE_SIZEX - 2*k;
+        let fieldWidth = TABLE_SIZEZ - 2*k;
 
         let n = 40;
         for (let x = -fieldLength / 2; x <= fieldLength / 2; x += fieldLength / n) {
-            this.add(new Cheerio(field, x, 0, fieldWidth / 2, TORUS_SIZE, TORUS_RADIUS));
-            this.add(new Cheerio(field, x, 0, -fieldWidth / 2, TORUS_SIZE, TORUS_RADIUS));
+            this.add(new Cheerio(x, 0, fieldWidth / 2, this.TORUS_SIZE, this.TORUS_RADIUS));
+            this.add(new Cheerio(x, 0, -fieldWidth / 2, this.TORUS_SIZE, this.TORUS_RADIUS));
         }
 
         n = 18;
         for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / n) {
-            this.add(new Cheerio(field, fieldLength / 2, 0, z, TORUS_SIZE, TORUS_RADIUS));
-            this.add(new Cheerio(field, -fieldLength / 2, 0, z, TORUS_SIZE, TORUS_RADIUS));
+            this.add(new Cheerio(fieldLength / 2, 0, z, this.TORUS_SIZE, this.TORUS_RADIUS));
+            this.add(new Cheerio(-fieldLength / 2, 0, z, this.TORUS_SIZE, this.TORUS_RADIUS));
         }
 
         k = this.TORUS_INSIDE_MARGIN + this.TORUS_SIZE;
-        fieldLength = this.TABLE_SIZEX - 2*k;
-        fieldWidth = this.TABLE_SIZEZ - 2*k;
+        fieldLength = TABLE_SIZEX - 2*k;
+        fieldWidth = TABLE_SIZEZ - 2*k;
 
         n = 30;
         for (let x = -fieldLength / 2; x <= fieldLength / 2; x += fieldLength / n) {
-            this.add(new Cheerio(field, x, 0, fieldWidth / 2, TORUS_SIZE, TORUS_RADIUS));
-            this.add(new Cheerio(field, x, 0, -fieldWidth / 2, TORUS_SIZE, TORUS_RADIUS));
+            this.add(new Cheerio(x, 0, fieldWidth / 2, this.TORUS_SIZE, this.TORUS_RADIUS));
+            this.add(new Cheerio(x, 0, -fieldWidth / 2, this.TORUS_SIZE, this.TORUS_RADIUS));
         }
 
         n = 8;
         for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / n) {
-            this.add(new Cheerio(field, fieldLength / 2, 0, z, TORUS_SIZE, TORUS_RADIUS));
-            this.add(new Cheerio(field, -fieldLength / 2, 0, z, TORUS_SIZE, TORUS_RADIUS));
+            this.add(new Cheerio(fieldLength / 2, 0, z, this.TORUS_SIZE, this.TORUS_RADIUS));
+            this.add(new Cheerio(-fieldLength / 2, 0, z, this.TORUS_SIZE, this.TORUS_RADIUS));
         }
     }
 }
