fb55bb5ead82926b547ecb7b1f5005814f352359
leo-epi <leonardo.epifanio@hotmail.com>
2017-10-07 19:28:55 +0100

    butter appears

--- public_html/js/butter/Butter.js
+++ public_html/js/butter/Butter.js
@@ -3,10 +3,8 @@
 class Butter extends Object3D {
     constructor(x, y, z) {
         super(x, y, z);
-        
         this.position.set(x, y+10, z);
-        
         this.butterBody = new ButterBody(0, 0, 0);
-
+        this.add(this.butterBody);
     }
 }
