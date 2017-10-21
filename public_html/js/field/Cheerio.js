36f8e08daf75f8ff5341c53efa425f4b5eaf2c81
duartegalvao <duartegalvao@tecnico.ulisboa.pt>
2017-10-21 17:09:13 +0100

    indent

--- public_html/js/field/Cheerio.js
+++ public_html/js/field/Cheerio.js
@@ -1,11 +1,24 @@
 class Cheerio extends SolidObject {
-  constructor(x, y, z, size, radius) {
-      super();
-      this.cheerioBody = new CheerioBody(0, 0, 0, size, radius);
-      this.boundingVolume = new BoundingSphere(0, 0, size + radius);
+    constructor(x, y, z, size, radius) {
+        super();
+        this.cheerioBody = new CheerioBody(0, 0, 0, size, radius);
+        this.boundingVolume = new BoundingSphere(0, 0, size + radius);
 
-      this.add(this.cheerioBody);
-      this.add(this.boundingVolume);
-      this.position.set(x, y, z);
-  }
+        this.add(this.cheerioBody);
+        this.add(this.boundingVolume);
+        this.position.set(x, y, z);
+    }
+
+    //override
+    update(delta) {
+        //this.checkMove(delta);
+    }
+
+    //override
+    collided(solidObject) {
+        if (solidObject instanceof Car ||
+            solidObject instanceof Cheerio) {
+            // TODO mover cheerio
+        }
+    }
 }
