2801110cc19e183caac4665e514d12a74e30a1e4
duartegalvao <duartegalvao@tecnico.ulisboa.pt>
2017-10-23 16:43:00 +0100

    cheerios menos retardads

--- public_html/js/field/Field.js
+++ public_html/js/field/Field.js
@@ -23,7 +23,7 @@ class Field {
         }
 
         n = 18;
-        for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / n) {
+        for (let z = - (fieldWidth / 2) + (fieldWidth / n); z <= (fieldWidth / 2) - (fieldWidth / n); z += fieldWidth / n) {
             this.cheerios.push(new Cheerio(fieldLength / 2, 0, z, this.SIZE, this.RADIUS));
             this.cheerios.push(new Cheerio(-fieldLength / 2, 0, z, this.SIZE, this.RADIUS));
         }
@@ -39,7 +39,7 @@ class Field {
         }
 
         n = 8;
-        for (let z = -fieldWidth / 2; z <= fieldWidth / 2; z += fieldWidth / n) {
+        for (let z = - (fieldWidth / 2) + (fieldWidth / n); z <= (fieldWidth / 2) - (fieldWidth / n); z += fieldWidth / n) {
             this.cheerios.push(new Cheerio(fieldLength / 2, 0, z, this.SIZE, this.RADIUS));
             this.cheerios.push(new Cheerio(-fieldLength / 2, 0, z, this.SIZE, this.RADIUS));
         }
